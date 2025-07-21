8768ef4c7cfaf294d46426ce801e0dc0e5fea285
Shahwatrajsingh <shashwatrajsing@gmail.com>
2025-07-21 23:53:58 +0530

    endpoints

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,8 @@ const corsOptions = {
     'http://localhost:3000',
     'http://localhost:5173',
     'http://127.0.0.1:3000',
-    'http://127.0.0.1:5173'
+    'http://127.0.0.1:5173',
+    'https://defi-dao-advisor.vercel.app/'
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
